0644211f0a3dfb72d343f3957111d76ae9e904af
qwed311 <qwed311@gmail.com>
2018-08-06 11:24:02 +0800

    initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const path= require('path');
+
+const app= express();
+const port = process.env.PORT || 3000;
+const publicPath= path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('app in running on port 300');
+});
